a68cb02c6675f9ca8eef47d71376df0fbcc30ab7
Malk97sc <malaksanchez09@gmail.com>
2025-10-13 08:04:50 -0500

    Patch Augmentation in SIDD

--- src/denoising/data/sidd_dataset.py
+++ src/denoising/data/sidd_dataset.py
@@ -7,24 +7,26 @@ from PIL import Image
 import os, random
 
 class SIDD(Dataset):
-    def __init__(self, root_dir, mode = 'train', img_size = 128):
+    def __init__(self, root_dir, mode = 'train', img_size = 128, patches = 50):
         """
         Args:
-            root_dir: Dir from 'noisy' and 'clean' dataset
+            root_dir: Path containing 'noisy' and 'clean' subdirectories.
             mode: 'train' or 'test'
             img_size: Size of images
+            patches: Number of random crops extracted from each image.
         """
 
         self.noisy_dir = os.path.join(root_dir, 'noisy')
         self.clean_dir = os.path.join(root_dir, 'clean')
         self.img_size = img_size
         self.mode = mode
+        self.patches_img = patches
 
         self.image_names = sorted(os.listdir(self.noisy_dir))
         self.to_tensor = transforms.ToTensor()
 
     def __len__(self):
-        return len(self.image_names)
+        return len(self.image_names) * self.patches_img
     
     def _read_rgb(self, path):
         bgr = cv.imread(path, cv.IMREAD_COLOR)
@@ -32,8 +34,9 @@ class SIDD(Dataset):
         return Image.fromarray(rgb)
 
     def __getitem__(self, idx):
-        noisy_path = os.path.join(self.noisy_dir, self.image_names[idx])
-        clean_path = os.path.join(self.clean_dir, self.image_names[idx])
+        img_idx = idx // self.patches_img
+        noisy_path = os.path.join(self.noisy_dir, self.image_names[img_idx])
+        clean_path = os.path.join(self.clean_dir, self.image_names[img_idx])
 
         noisy = self._read_rgb(noisy_path)
         clean = self._read_rgb(clean_path) 
@@ -55,7 +58,8 @@ class SIDD(Dataset):
                 noisy = TF.vflip(noisy); clean = TF.vflip(clean)
         else:
             crop = transforms.CenterCrop(self.img_size)
-            noisy = crop(noisy); clean = crop(clean)
+            noisy = crop(noisy)
+            clean = crop(clean)
 
         noisy_tensor = self.to_tensor(noisy) #[0,1]
         clean_tensor = self.to_tensor(clean) #[0,1]
